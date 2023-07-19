8c6d0db71b1f7ce266de716ed32282eb75c108b8
RexRobles08 <133176406+RexRobles08@users.noreply.github.com>
2023-07-19 18:06:11 -0400

    first commit

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Database Setup
+let mongoose = require('mongoose');
+let DB = require('./config/db');
+
+mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', () => {
+  console.log('Node.JS is successfully connected to MongoDB.')
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactRouter = require('./routes/contact');
+var projectRouter = require('./routes/project');
 
 var app = express();
 
@@ -22,6 +36,8 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
+app.use('/project', projectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.get('/services', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+/*
 // insert name to the home page
 router.post('/contact', (req, res, next) => {
   //getting data from form
@@ -33,5 +34,5 @@ router.post('/contact', (req, res, next) => {
   console.log('sdfghjklghjkl;fghjkl;fghjkl ',newName)
   res.render('index', {title : 'home' , username : 'Hi ' +newName+', '})
 });
-
+*/
 module.exports = router;
